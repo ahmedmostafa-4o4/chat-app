2c6eb24ef64ff8c5d6fd17125ceeed403affaf2b
Ahmed Mostafa <lofylofy56@gmail.com>
2025-09-03 20:54:54 +0300

    ..

--- src/app/(chat)/page.tsx
+++ src/app/(chat)/page.tsx
@@ -1,10 +1,15 @@
 import Chat from "@/components/Chat";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Chat App",
   description: "...",
 };
 export default function Home() {
-  return <Chat />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Chat />
+    </Suspense>
+  );
 }
